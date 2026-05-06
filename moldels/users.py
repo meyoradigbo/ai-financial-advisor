da050e50ee516ce10812f94684288ca5d610af4b
Charles Meyor Adigbo <39588845+meyoradigbo@users.noreply.github.com>
2026-05-06 19:14:53 +0000

    Delete moldels directory
    wrong name

--- moldels/users.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from backend.database import Base
-
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
